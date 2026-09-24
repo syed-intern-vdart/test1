711ba1a95f9a2e68d383d32e2a4eaa7bdd0ddb05
syed-intern-vdart <timesumma1@gmail.com>
2026-09-24 21:25:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask("__name__")
 @app.route("/")
 def home():
-    return <h1>hello World !!</h1>
+    return "<h1>hello World !!</h1>"
 if __name__="__main__":
     app.run(debug=True)
